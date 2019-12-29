5e121e224060d1c1c4ab4c8fb956fb2779014a1b
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2019-12-30 02:08:28 +0800

    Update fibonacci.c

--- Number Theory/fibonacci.c
+++ Number Theory/fibonacci.c
@@ -1,10 +1,10 @@
 //input:n
 //output:the n^th fibonacci number
-//Theta(log(N)) but the matirx multiplication is very naive
 
 #include<stdio.h>
 #include<stdlib.h>
 
+//Theta(log(N)) but the matirx multiplication is very naive
 int* matrix_multi(int *a,int *b,int m,int n,int p){
         int *res=(int*)malloc(sizeof(int)*m*p);
         for(int r=0;r<m*p;r++)
@@ -40,6 +40,55 @@ int fib(int n){
         free(tmp);
         return ret;
 }
+
+//naive version
+int FacIter(int n){
+        int res=1;
+        for(int i=1;i<=n;i++){
+                res=res*i;
+        }
+        return res;
+}
+
+//naive version
+int FacRec(int n){
+        if (n==1)
+                return 1;
+        else
+                return n*FacRec(n-1);
+}
+
+//use multiplication to calculate
+//Theta(log(N))
+int multi_fib(int n){
+        int a=0,b=1,c=1,d=1;
+        int res_a=0,res_b=1,res_c=1,res_d=1;
+        int tmp_a,tmp_b,tmp_c,tmp_d;
+        while(n){
+                if(n&1){
+                        tmp_a=res_a*a+res_b*c;
+                        tmp_b=res_a*b+res_b*d;
+                        tmp_c=res_c*a+res_d*c;
+                        tmp_d=res_c*b+res_d*d;
+                        res_a=tmp_a;
+                        res_b=tmp_b;
+                        res_c=tmp_c;
+                        res_d=tmp_d;
+                }
+                        tmp_a=a*a+b*c;
+                        tmp_b=a*b+b*d;
+                        tmp_c=a*c+c*d;
+                        tmp_d=b*c+d*d;
+                        a=tmp_a;
+                        b=tmp_b;
+                        c=tmp_c;
+                        d=tmp_d;
+                n>>=1;
+        }
+        return res_a;
+}
+
+
 
 int main(){
         int n;
