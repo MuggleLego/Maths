62acef9f50c03bed5903abd7b018ca6c211bea91
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-01-03 01:20:34 +0800

    Update Fast_modulus_exponentiation.c

--- Number Theory/Fast_modulus_exponentiation.c
+++ Number Theory/Fast_modulus_exponentiation.c
@@ -1,1 +1,8 @@
-use Euler's theory
+//date:2020.1.3
+//input:positive integer a,b,n
+//output:a^b mod n(less than n)
+//use Euler's theory
+
+//assume p is a prime
+int modulus_exp(int a,int b,int p){
+  
