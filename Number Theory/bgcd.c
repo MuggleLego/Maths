5bd916ee196b2c5954ade6ec6d6af07bee56b888
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-09-15 12:32:03 +0800

    Update bgcd.c

--- Number Theory/bgcd.c
+++ Number Theory/bgcd.c
@@ -16,13 +16,39 @@ int min(int a,int b){
     return a>b?b:a;
 }
 
-int gcd(int a,int b){
+void swap(int* a,int* b){
+    int tmp=*a;
+    *a=*b;
+    *b=tmp;
+}
+
+int rec_bin_gcd(int a,int b){
     if(!a) return b;//base case
     if(!b) return a;//base case
     if(!(a&1) && !(b&1)) return gcd(a>>1,b>>1)>>1;//case 1
     else if(!(a&1)) return gcd(a>>1,b);//case 2
     else if(!(b&1)) return gcd(a,b>>1);//case 2
     else return gcd(abs(a-b)>>1,min(a,b)); //case 3
+}
+
+int iter_bin_gcd(int a,int b){
+    int k=0;
+    while((a|b)&1==0){
+        a>>=1;
+        b>>=1;//if a,b are evens
+        k++;//gcd(a,b)=2^k*gcd(a>>k,b>>k)
+    }
+    while(!(a&1)){
+        a>>=1;//a is odd
+    }
+    while(b){
+        while(!(b&1)){
+            b>>=1;//a is odd,so gcd(a,b)=gcd(a,b/2),b is odd eventually
+        }
+        if(a>b) swap(&a,&b);//from then on,b>=a
+        b-=a;//gcd(a,b/2)=gcd(b-a,b/2),b-a is even,loop.
+    }
+    return a<<k;//b=0,a=gcd(a>>k,b>>k),res=2^k*gcd(a>>k,b>>k)
 }
 
 int main(){
