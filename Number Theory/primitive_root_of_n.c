a5ed8cfdcd764b0a029aa2f5d4dea95da1d41bc1
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-10-15 19:28:38 +0800

    Create primitive_root_of_n.c

---
+++ Number Theory/primitive_root_of_n.c
@@ -0,0 +1,6 @@
+//date:2020.10.15
+//input:an arbitrary integer n
+//output:the smallet primitive root of n
+//distinguish with primitive of prime p:
+//we need to decide whether gcd(a,n)==1,if gcd(a,n) !=1,that a can't be a primitive root of n
+
